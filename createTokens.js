60be6d280aeff1dd369889e5404fe750f3f2902f
rico <ricoruiaz1988@gmail.com>
2020-05-27 00:11:59 -0700

    Merge branch 'master' of https://github.com/MyEtherWallet/ethereum-lists

---
+++ createTokens.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const Web3 = require('web3');
+const fetch = require('node-fetch');
+const utils = Web3.utils;
+const tokensDirectory = './src/tokens/eth/';
+const notInListPath = './notinlist.json';
+const notInList = JSON.parse(fs.readFileSync(notInListPath));
+const api = 'https://api.coingecko.com/api/v3/coins/ethereum/contract'; 
+const node = 'https://nodes.mewapi.io/rpc/eth';
+const abi = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint8"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  }];
+const web3 = new Web3(node);
+async function createToken() {
+  for (let index = 98; index < 144; index++) {
+  // for (let index = 101; index < 200; index++) {
+    const contract = new web3.eth.Contract(abi, notInList[index]);
+    const decimal = await contract.methods.decimals().call().catch(res => {
+      console.log(notInList[index], res)
+    });
+    const tokenInfo = await fetch(`${api}/${notInList[index]}`).then(response => {
+      return response.json();
+    }).catch(err => {
+      console.log(index, notInList[index])
+      console.log(err);
+    });
+    const homepage = tokenInfo.hasOwnProperty('links') ? tokenInfo.links.hasOwnProperty('homepage') ? tokenInfo.links.homepage[0] : '' : '';
+    const tokenTemp =
+        {
+          "symbol": tokenInfo.symbol.toUpperCase(),
+          "name": tokenInfo.name,
+          "type": "ERC20",
+          "address": utils.toChecksumAddress(notInList[index]),
+          "ens_address": "",
+          "decimals": Number(decimal),
+          "website": homepage,
+          "logo": {
+              "src": "",
+              "width": "",
+              "height": "",
+              "ipfs_hash": ""
+          },
+          "support": {
+              "email": "",
+              "url": ""
+          },
+          "social": {
+              "blog": "",
+              "chat": "",
+              "facebook": "",
+              "forum": "",
+              "github": "",
+              "gitter": "",
+              "instagram": "",
+              "linkedin": "",
+              "reddit": "",
+              "slack": "",
+              "telegram": "",
+              "twitter": "",
+              "youtube": ""
+          }
+      };
+        fs.writeFileSync(
+          `${tokensDirectory}/${utils.toChecksumAddress(
+            notInList[index].replace('.json', '')
+          )}.json`,
+          JSON.stringify(tokenTemp)
+    );
+  }
+  
+}
+createToken();
---
+++ generateMissingTokenListFromIcons.js
@@ -0,0 +1,48 @@
+const ethList = './dist/tokens/eth/tokens-eth.json';
+const ethIcons = './src/icons';
+const web3 = require('web3');
+const utils = web3.utils;
+const fs = require('fs');
+
+function generateMissingToken() {
+  const icons = fs.readdirSync(ethIcons);
+  const list = JSON.parse(fs.readFileSync(ethList, 'utf8'));
+  const exclusion = [
+    '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    '0xa66d83716c7cfe425b44d0f7ef92de263468fb3d',
+    '0x97208bf5dc25e6fd4719cfc2a3c1d1a59a974c3b',
+    '0x1c5b760f133220855340003b43cc9113ec494823',
+  ]
+
+  const addressOnly = [];
+  const notInList = [];
+  icons.forEach(icon => {
+    const idxOf = icon.indexOf('-');
+    const getAddr = icon.substring(idxOf + 1, icon.length - 4);
+    if(getAddr.length !== 42) {
+      const actualAddress = getAddr.substring(getAddr.indexOf('0x'), 42);
+      addressOnly.push(actualAddress);
+    } else {
+      addressOnly.push(getAddr);
+    }
+  })
+  
+  addressOnly.forEach(addr => {
+    if(utils.isAddress(addr)) {
+      const inExclusionList = exclusion.find(item => {
+        return utils.toChecksumAddress(item) === utils.toChecksumAddress(addr);
+      })
+      const found = list.find(item => {
+        if(addr.substring(0, 2) === '0x' && addr.length === 42) {
+          return utils.toChecksumAddress(item.address) === utils.toChecksumAddress(addr);
+        }
+      })
+      if(!found && addr.substring(0, 2) === '0x' && addr.length === 42 && !inExclusionList) notInList.push(addr);
+    } else {
+      console.log(addr);
+    }
+  });
+  fs.writeFileSync('notinlist.json', JSON.stringify(notInList))
+}
+
+generateMissingToken();
